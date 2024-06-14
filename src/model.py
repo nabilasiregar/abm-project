6de55a3e497486ac591270d412148126f5844e8d
AdrianRue <aruesink13@gmail.com>
2024-06-14 17:54:47 +0200

    Added inital data collection(not complete)

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 import mesa
 from agent import EconomicAgent, CopAgent
+from mesa.datacollection import DataCollector
 
 class EconomicModel(mesa.Model):
     def __init__(self, num_econ_agents, initial_cops=0, width=10, height=10, election_frequency = 20, sentence_length = 15, interaction_memory = 5):
@@ -23,10 +24,12 @@ class EconomicModel(mesa.Model):
         self.votes = 0
         
         #counters, this will have to be replaced with a datacollector
-        self.num_crimes_committed = 0
+        self.num_crimes_committed_turn = 0
         self.num_arrests_made = 0
         self.total_stolen = 0
         self.total_trade_income = 0
+        self.trades_made_turn = 0
+        self.arrested_agents = 0
         self.steps = 0
         
         # create agents
@@ -43,7 +46,40 @@ class EconomicModel(mesa.Model):
             self.schedule.add(c)
             self.grid.place_agent(c, (x, y))
 
+
+        #For Data collection
+        self.datacollector = DataCollector(
+            {"No. crimes committed this turn": lambda m: self.num_crimes_committed_turn,
+            "Total trades made this turn": lambda m: self.trades_made_turn,
+            "Arrested agents": lambda m: self.arrested_agents}
+        )
+
+        self.running = True
+        self.datacollector.collect(self)
+
+    def collect_data(self):
+
+        self.num_crimes_committed_turn = 0
+        self.trades_made_turn = 0
+        self.arrested_agents = 0
+
+
+
+        for agent in self.agents:
+
+            if isinstance(agent, EconomicAgent):
+                if agent.has_committed_crime_this_turn:
+                    self.num_crimes_committed_turn += 1
+                if agent.has_traded_this_turn:
+                    self.trades_made_turn += 1
+                if agent.is_arrested:
+                    self.arrested_agents += 1
+                
+
+
     def step(self):
+
+
         self.steps += 1
         self.schedule.step()
         if (self.steps-1)%self.election_frequency == 0 and self.steps != 1:
@@ -64,8 +100,14 @@ class EconomicModel(mesa.Model):
                 y = self.random.randrange(self.grid.height)
                 self.schedule.add(c)        
                 self.grid.place_agent(c, (x, y))
-            else:
+            elif len(cops) > 0:
                 cops[0].remove()
                 self.schedule.remove(cops[0])
             # reset the votes
             self.votes = 0
+
+
+        self.collect_data()
+
+        
+        self.datacollector.collect(self)
--- src/run.py
+++ src/run.py
@@ -1,6 +1,10 @@
 import mesa
 from model import EconomicModel
 
-model = EconomicModel(num_econ_agents=3, initial_cops=1, width=3, height = 3, election_frequency=20, tax_rate=0.01)
-for i in range(30):
+model = EconomicModel(num_econ_agents=5, initial_cops=1, width=3, height = 3, election_frequency=20)
+for i in range(400):
     model.step()
+
+data = model.datacollector.get_model_vars_dataframe()
+
+print(data)
