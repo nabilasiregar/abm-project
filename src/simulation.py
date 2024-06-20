ea31050fba7ee9f20fc2436b5cc8619c1435d990
Kenia López <104690170+KeniaLS@users.noreply.github.com>
2024-06-20 16:52:04 +0200

    Prosperity/Elec freq simulation

--- src/simulation.py
+++ src/simulation.py
@@ -14,7 +14,7 @@ def run_varying_cops(cops_levels, fixed_sentence, num_steps, num_agents, width, 
             for step in range(num_steps):
                 model.step()
                 step_data = model.datacollector.get_model_vars_dataframe().iloc[-1].to_dict()
-                step_data.update({"Step": step, "iteration": i + 1, "value": cops})
+                step_data.update({"Step": step, "iteration": i + 1, "param": "cops", "value": cops})
                 results.append(step_data)
     return pd.DataFrame(results)
 
@@ -28,7 +28,7 @@ def run_varying_sentences(sentence_lengths, fixed_cops, num_steps, num_agents, w
             for step in range(num_steps):
                 model.step()
                 step_data = model.datacollector.get_model_vars_dataframe().iloc[-1].to_dict()
-                step_data.update({"Step": step, "iteration": i + 1, "value": length})
+                step_data.update({"Step": step, "iteration": i + 1, "param": "sentence", "value": length})
                 results.append(step_data)
     return pd.DataFrame(results)
 
@@ -42,10 +42,9 @@ def run_varying_election_frequencies(election_frequencies, fixed_cops, fixed_sen
             for step in range(num_steps):
                 model.step()
                 step_data = model.datacollector.get_model_vars_dataframe().iloc[-1].to_dict()
-                step_data.update({"Step": step, "iteration": i + 1, "value": freq})
+                step_data.update({"Step": step, "iteration": i + 1, "param": "elec_freq", "value": freq})
                 results.append(step_data)
     return pd.DataFrame(results)
-
 
 def run_varying_prosperity(prosperity_levels, fixed_cops, fixed_sentence, num_steps, num_agents, width, height, election_frequency, interaction_memory, iterations):
     results = []
@@ -63,24 +62,24 @@ def run_varying_prosperity(prosperity_levels, fixed_cops, fixed_sentence, num_st
 
 def main():
     parser = argparse.ArgumentParser(description="Run ABM simulation")
-    parser.add_argument("type", choices=['vary_cops', 'vary_sentence'], help="Type of simulation to run: 'vary_cops' or 'vary_sentence'")
-    parser.add_argument("--steps", type=int, default=200, help="Number of steps in the simulation")
-    parser.add_argument("--num_agents", type=int, default=50, help="Number of economic agents")
+    parser.add_argument("type", choices=['vary_cops', 'vary_sentence', 'vary_election_freq', 'vary_prosperity'], help="Type of simulation to run: 'vary_cops' or 'vary_sentence' or 'vary_election_freq' or 'vary_prosperity'")
+    parser.add_argument("--steps", type=int, default=1000, help="Number of steps in the simulation")
+    parser.add_argument("--num_agents", type=int, default=70, help="Number of economic agents")
     parser.add_argument("--width", type=int, default=10, help="Grid width")
     parser.add_argument("--height", type=int, default=10, help="Grid height")
-    parser.add_argument("--election_frequency", type=int, default=20, help="Election frequency")
+    parser.add_argument("--election_frequency", type=int, default=70, help="Election frequency")
     parser.add_argument("--interaction_memory", type=int, default=5, help="Interaction memory length")
-    parser.add_argument("--iterations", type=int, default=30, help="Number of iterations to run each simulation setup")
+    parser.add_argument("--iterations", type=int, default=50, help="Number of iterations to run each simulation setup")
     args = parser.parse_args()
 
     if args.type == 'vary_cops':
         results = run_varying_cops([1, 3, 5, 7, 10], 15, args.steps, args.num_agents, args.width, args.height, args.election_frequency, args.interaction_memory, args.iterations)
     elif args.type == 'vary_sentence':
-        results = run_varying_sentences([5, 10, 15, 20, 25], 5, args.steps, args.num_agents, args.width, args.height, args.election_frequency, args.interaction_memory, args.iterations)
+        results = run_varying_sentences([5, 10, 15, 20, 25], 2, args.steps, args.num_agents, args.width, args.height, args.election_frequency, args.interaction_memory, args.iterations)
     elif args.type == 'vary_election_freq':
-        results = run_varying_election_frequencies([10, 20, 30, 40, 50], 5, 15, args.steps, args.num_agents, args.width, args.height, args.interaction_memory, args.iterations)
+        results = run_varying_election_frequencies([10, 20, 30, 40, 50, 60, 70], 2, 15, args.steps, args.num_agents, args.width, args.height, args.interaction_memory, args.iterations)
     elif args.type == 'vary_prosperity':
-        results = run_varying_prosperity([0.01, 0.05, 1, 1.5, 2, 2.5, 3], 5, 15, args.steps, args.num_agents, args.width, args.height, args.election_frequency, args.interaction_memory, args.iterations)
+        results = run_varying_prosperity([0.1, 0.2, 0.3, 0.4, 0.5, 1], 2, 15, args.steps, args.num_agents, args.width, args.height, args.election_frequency, args.interaction_memory, args.iterations)
     results.to_csv(f"../results/{args.type}_results.csv", index=False)
     print(f"Saved simulation results to {args.type}_results.csv")
 
