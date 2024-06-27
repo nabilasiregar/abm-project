19ba001a268f7ec5d9ec4e3fc8f9137b061fabe1
nabilasiregar <52696885+nabilasiregar@users.noreply.github.com>
2024-06-27 15:46:21 +0200

    some changes

--- src/parallel_run.py
+++ src/parallel_run.py
@@ -24,10 +24,10 @@ def run_simulation(params, max_steps, iteration):
     # Collect model-level data
     model_results = model.datacollector.get_model_vars_dataframe()
     model_results["iteration"] = iteration + 1
-    model_results["election_frequency"] = params["election_frequency"]
-    model_results["sentence_length"] = params["sentence_length"]
-    model_results["interaction_memory"] = params["interaction_memory"]
-    model_results["risk_aversion_std"] = params["risk_aversion_std"]
+    # model_results["election_frequency"] = params["election_frequency"]
+    # model_results["sentence_length"] = params["sentence_length"]
+    # model_results["interaction_memory"] = params["interaction_memory"]
+    # model_results["risk_aversion_std"] = params["risk_aversion_std"]
     for key, value in params.items():
         model_results[key] = value
     
@@ -39,7 +39,7 @@ def run_simulation(params, max_steps, iteration):
 
 def run():
     num_samples = 10
-    num_iterations = 30
+    num_iterations = 200
     max_steps = 500
     
     bounds = {
@@ -52,18 +52,18 @@ def run():
     params_list = [
         {
             "num_econ_agents": 200, 
-            "initial_cops": 2, 
+            "initial_cops": 8, 
             "width": 20, 
             "height": 20, 
-            "election_frequency": elect, 
-            "sentence_length": sl,
-            "interaction_memory": im,
-            "risk_aversion_std": ra
+            "election_frequency": 50, 
+            "sentence_length": 20,
+            "interaction_memory": 50,
+            "risk_aversion_std": 0.3
         }
-        for elect in np.linspace(*bounds["election_frequency"], num=num_samples, dtype=int)
-        for sl in np.linspace(*bounds["sentence_length"], num=num_samples, dtype=int)
-        for im in np.linspace(*bounds["interaction_memory"], num=num_samples, dtype=int)
-        for ra in np.linspace(*bounds["risk_aversion_std"], num=num_samples)
+        # for elect in np.linspace(*bounds["election_frequency"], num=num_samples, dtype=int)
+        # for sl in np.linspace(*bounds["sentence_length"], num=num_samples, dtype=int)
+        # for im in np.linspace(*bounds["interaction_memory"], num=num_samples, dtype=int)
+        # for ra in np.linspace(*bounds["risk_aversion_std"], num=num_samples)
     ]
     
     # -1 means use all available CPU
@@ -74,17 +74,17 @@ def run():
     )
     
     model_results = []
-    agent_results = []
+    # agent_results = []
     
-    for model_df, agent_df in results:
+    for model_df in results:
         model_results.append(model_df)
-        agent_results.append(agent_df)
+        # agent_results.append(agent_df)
     
     model_results_df = pd.concat(model_results, ignore_index=True)
-    agent_results_df = pd.concat(agent_results, ignore_index=True)
+    # agent_results_df = pd.concat(agent_results, ignore_index=True)
     
-    model_results_df.to_csv('results/ofat_model_results.csv', index=False)
-    agent_results_df.to_csv('results/ofat_agent_results.csv', index=False)
+    model_results_df.to_csv('results/default_results_200iterations_200agents_8cops_20gridsize.csv', index=False)
+    # agent_results_df.to_csv('results/ofat_agent_results.csv', index=False)
 
 if __name__ == '__main__':
     run()
